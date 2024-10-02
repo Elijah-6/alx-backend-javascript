6a448a0af58eab33fcf8d2a2513e053b8e62e3e2
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-10-02 11:11:19 +0000

    8-try.js

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,10 +1,4 @@
 export default function divideFunction(numerator, denominator) {
-  let result = 0;
-  try {
-    if (denominator === 0) throw Error('cannot divide by 0');
-    result = numerator / denominator;
-  } catch (err) {
-    console.log(err);
-  }
-  return result;
+  if (denominator === 0) throw Error('cannot divide by 0');
+  return (numerator / denominator);
 }
