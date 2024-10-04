fcb583eadfb0f529571dd4abb14f96a71dcf9389
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-10-04 15:54:29 +0000

    eslinting

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -4,12 +4,10 @@ export default class HolbertonClass {
     this._location = location;
   }
 
-    // When the class is cast to a number, return the size
   valueOf() {
-   return this._size;
+    return this._size;
   }
 
-    // When the class is cast to a string, return the location
   toString() {
     return this._location;
   }
