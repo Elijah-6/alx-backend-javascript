0f4b6d392157a56eddb18ba2001482be35014f43
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-10-04 14:20:51 +0000

    on it

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,55 +1,11 @@
-export default class HolbertonCourse {
-  constructor(name, length, students) {
-    if (typeof name !== 'string') {
-      throw TypeError('name must be a String');
-    }
-    if (typeof length !== 'number') {
-      throw TypeError('length must be a Number');
-    }
-    if (!Array.isArray(students)) {
-      throw TypeError('students must be an Array');
-    }
-    if (students.some((student) => typeof student !== 'string')) {
-      throw TypeError('students must be an Array of Strings');
-    }
-    this._name = name;
-    this._length = length;
-    this._students = students;
-  }
+import ClassRoom from './0-classroom';
 
-  get name() {
-    return this._name;
-  }
-	
-  set name(value) {
-    if (typeof value !== 'string') {
-      throw TypeError('name must be a String');
-    }
-    this._name = value;
-  }
-	
-  get length() {
-    return this._length;
-  }
-	
-  set length(value) {
-    if (typeof value !== 'number') {
-      throw TypeError('length must be a Number');
-    }
-    this._length = value;
-  }
-	
-  get students() {
-    return this._students;
-  }
-	
-  set students(value) {
-    if (!Array.isArray(value)) {
-      throw TypeError('students must be an Array');
-    }
-    if (value.some((student) => typeof student !== 'string')) {
-      throw TypeError('students must be an Array of Strings');
-    }
-    this._students = value;
-  }
+export default function initializeRooms() {
+  return (
+    [
+      new ClassRoom(19),
+      new ClassRoom(20),
+      new ClassRoom(34),
+    ]
+  );
 }
