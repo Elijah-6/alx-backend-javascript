7ab36d073db22f620b933da9c2c6b54860a1e62b
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-10-08 11:25:38 +0000

    10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,5 @@
 export default function updateUniqItems(groceries) {
+  if (!(groceries instanceof Map)) throw Error('Cannot process');
   groceries.set('Pasta', 100);
   groceries.set('Rice', 100);
   return groceries;
