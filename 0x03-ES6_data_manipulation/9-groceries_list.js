b95f0832d3a70f17ac44714f503b9f508b26f0cb
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-10-08 11:11:01 +0000

    9-groceries_list.js

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+export default function groceriesList() {
+  // Create a new Map
+  const groceries = new Map();
+  // Add the following key-value pairs to the Map
+  groceries.set('Apples', 10);
+  groceries.set('Tomatoes', 10);
+  groceries.set('Pasta', 1);
+  groceries.set('Rice', 1);
+  groceries.set('Banana', 5);
+  return groceries;
+}
