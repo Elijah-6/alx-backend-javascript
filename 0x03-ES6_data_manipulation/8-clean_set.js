fd0ea6cf7e6b67329bbe0adc1a2d14d9163dadc4
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-10-08 10:53:17 +0000

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -4,6 +4,6 @@ export default function cleanSet(set, startString) {
   }
   const filteredValues = [...set]
     .filter((value) => value.startsWith(startString))
-    .map((value) => value.replace(startString, ''));
+    .map((value) => value.slice(startString.length));
   return filteredValues.join('-');
 }
